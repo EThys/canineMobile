c6f619f10d3c2201cdd68d15af172de2136d757b
Muzola Ethberg <ethbergmuzola500@gmail.com>
2025-01-23 10:55:03 +0100

    Feat: add resources

---
+++ lib/resources/vectors.dart
@@ -0,0 +1,10 @@
+part of 'resources.dart';
+
+class Vectors {
+  Vectors._();
+
+  static const String facebook = 'assets/vectors/facebook.svg';
+  static const String background = 'assets/vectors/background.png';
+  static const String google = 'assets/vectors/google.svg';
+  static const String asyco = 'assets/vectors/acyco.png';
+}
