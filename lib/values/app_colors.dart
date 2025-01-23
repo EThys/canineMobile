b7d24ecba2db61aa4e01eabcde387752cd5372aa
Muzola Ethberg <ethbergmuzola500@gmail.com>
2025-01-23 10:53:41 +0100

    Feat: add values

---
+++ lib/values/app_colors.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  const AppColors._();
+
+  static const Color primaryColor = Color(0xFFFFEE58);
+  static const Color darkBlue = Color(0xff63cdf5);
+  static const Color darkerBlue = Color(0xfffa4a60);
+  static const Color darkestBlue = Color(0xff63cdf5);
+
+  static const List<Color> defaultGradient = [
+    darkBlue,
+    darkerBlue,
+    darkestBlue,
+  ];
+}
---
+++ lib/values/app_strings.dart
@@ -0,0 +1,69 @@
+class AppStrings {
+  const AppStrings._();
+
+  static const String loginAndRegister = 'Login and Register UI';
+  static const String uhOhPageNotFound = 'uh-oh!\nPage not found';
+  static const String registerApp = 'Enregistrement du personnel';
+  static const String register = 'Inscription';
+  static const String login = 'Connexion';
+  static const String createYourAccount = 'Créer un compte';
+  static const String doNotHaveAnAccount = "Vous n'avez pas de compte?";
+  static const String facebook = 'Facebook';
+  static const String google = 'Google';
+  static const String signInToYourNAccount = 'Se connecter Au système';
+  static const String home = 'Welcome to Acyco';
+  static const String appName = 'ACYCO';
+  static const String iHaveAnAccount = 'Ai-je un compte?';
+  static const String forgotPassword = 'Mot de passe oublié ?';
+  static const String orLoginWith = 'ou avec';
+
+  static const String loggedIn = 'Logged In!';
+  static const String registrationComplete = 'Registration Complete!';
+
+  static const String name = 'Nom';
+  static const String pleaseEnterName = 'Entrer votre nom';
+  static const String invalidName = 'Invalid Name';
+
+  static const String email = 'Email';
+  static const String pleaseEnterEmailAddress = 'Entrer votre adresse email';
+  static const String invalidEmailAddress = 'Address email invalide';
+
+  static const String password = 'Mot de passe';
+  static const String pleaseEnterPassword = 'Entrer votre mot de passe';
+  static const String invalidPassword = 'Mot de passe incorrect';
+
+  static const String postnom = 'Postnom';
+  static const String pleaseEnterPostnom = 'Votre postnon est requis';
+  static const String invalidPostnom = 'Postnom invalide';
+
+  static const String prenom = 'prenom';
+  static const String pleaseEnterPrenom = 'Votre prenom est requis';
+  static const String invalidPrenom = 'Prenom invalide';
+
+  static const String adresse = 'adresse';
+  static const String pleaseEnterAdresse = 'Votre adresse est requis';
+  static const String invalidAdresse= 'Adresse invalide';
+
+  static const String profession = 'profession';
+  static const String pleaseEnterProfession = 'Votre profession est requis';
+  static const String invalidProfession = 'Profession invalide';
+
+  static const String userName = 'UserName';
+  static const String pleaseEmterUserName= 'Un username est requis';
+  static const String invalidUserName= 'username invalide';
+
+  static const String genre = 'genre';
+  static const String pleaseEnterGenre = 'Votre genre est requis';
+  static const String invalidGenre = 'Genre invalide';
+
+  static const String phone = 'phone';
+  static const String pleaseEnterPhone = 'Votre telephone est requis';
+  static const String invalidPhone = 'Numero de telephone invalide';
+
+  static const String DateNaissance = 'DateNaissance';
+  static const String pleaseEnterDateNaissance = 'Ce champ est requis';
+
+  static const String confirmPassword = 'Confirmez mot de passe';
+  static const String pleaseReEnterPassword = 'Please, Re-Enter Password';
+  static const String passwordNotMatched = 'Mot de passe different!';
+}
