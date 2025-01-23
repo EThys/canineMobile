ebdba51aa0b1890f9279a07765fcd1c5214184e8
Muzola Ethberg <ethbergmuzola500@gmail.com>
2025-01-23 10:42:59 +0100

    Feat: Point d'entre de l'application

---
+++ lib/apps/MonApplication.dart
@@ -0,0 +1,39 @@
+import 'package:canineappadmin/controllers/SearchController.dart';
+import 'package:canineappadmin/utils/helpers/snackbar_helper.dart';
+import 'package:canineappadmin/values/app_theme.dart';
+import 'package:flutter/material.dart';
+import 'package:get_storage/get_storage.dart';
+import '../controllers/AuthentificationCtrl.dart';
+import 'package:provider/provider.dart';
+import '../utils/Routes.dart';
+import '../utils/RoutesManager.dart';
+import '../utils/StockageKeys.dart';
+
+class MonApplication extends StatelessWidget {
+  var _stockage = GetStorage();
+
+  @override
+  Widget build(BuildContext context) {
+    bool isFirstLaunch = _stockage.read(StockageKeys.firstLaunchKey) ?? true;
+    var token = _stockage.read(StockageKeys.tokenKey);
+
+    if (isFirstLaunch) {
+      _stockage.write(StockageKeys.firstLaunchKey, false);
+    }
+
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => AuthentificationCtrl(stockage: _stockage)),
+        ChangeNotifierProvider(create: (_) => RechercheController(stockage: _stockage)),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: AppTheme.themeData,
+        onGenerateRoute: RoutesManager.route,
+        themeMode: ThemeMode.light,
+        scaffoldMessengerKey: SnackbarHelper.key,
+        initialRoute:  Routes.splashscreenRoute,
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:canineappadmin/apps/MonApplication.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:get_storage/get_storage.dart';
+
+import 'login_register_app.dart';
+
+void main() async {
+  await GetStorage.init();
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setSystemUIOverlayStyle(
+    const SystemUiOverlayStyle(statusBarIconBrightness: Brightness.light),
+  );
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]).then(
+    (_) => runApp( MonApplication()),
+  );
+}
